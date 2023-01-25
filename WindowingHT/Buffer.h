d727d8ba400cb6178c22d24a7508ee2b54d8507e
Amulassano <115482563+Amulassano@users.noreply.github.com>
2023-01-25 16:16:18 +0100

    Create Buffer.h

---
+++ WindowingHT/Buffer.h
@@ -0,0 +1,35 @@
+#define MM 1000
+#define String_Lenght 10000
+
+typedef struct List *list;
+typedef struct  node *link;
+
+typedef struct wind{ // [o,c)
+    int o; //opening
+    int c; //closing -> opened
+}window;
+
+typedef struct content{
+    char e[String_Lenght];
+    int ts;
+}content;
+
+struct node{
+    window w; //finestra
+    content *c; //content:(vettore di (e,ts)
+    int nc; //numero di oggetti content nella finestra
+};
+
+struct List{ //Buffer
+    link *head; //prima finestra
+    int N; //nrappresenta la casella successiva all'ultimo arrivo
+    int M; //indicatore della prima cella occupata
+    int c; //prima chiusura
+    int count; //numero caselle passate nel tempo
+};
+
+list x;
+
+
+void allocaBuffer(int ts);
+void addToBuffer(char e[String_Lenght], int ts, int l);
