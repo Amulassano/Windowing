dd1466fd73957b738c16fcbd6e77866c1f3a38b7
Amulassano <115482563+Amulassano@users.noreply.github.com>
2023-02-08 12:37:59 +0100

    Update main.c

--- WindowingHT/main.c
+++ WindowingHT/main.c
@@ -1,9 +1,20 @@
-#include "Windowing.h"
+#include "AlterazioneWindowing.h"
+#include <stdio.h>
 
 int main(){
 
-    for (int i = 1; i <= 5000; i++) {
-        windowing("ss",i);
+    FILE *ff = fopen("log.txt","w");
+    fclose(ff);
+    ff = fopen("evict.txt","w");
+    fclose(ff);
+
+    char h[1][20];
+    for (int i=0;i<1;i++)
+        for (int l=0;l<20;l++)
+            h[i][l]='h';
+
+    for (int i=1;i<500;i++){
+        windowing(h,i);
     }
 
     return 0;
