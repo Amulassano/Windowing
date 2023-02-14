30710518e31b47705407e2befc99df86b243d5f1
Amulassano <115482563+Amulassano@users.noreply.github.com>
2023-02-14 16:17:59 +0100

    Update Buffer.c

--- WindowingHT/Buffer.c
+++ WindowingHT/Buffer.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-int t = 0; //scelgo un tempo iniziale
-int slid = 5; //scelgo un slide per le finestre
+int t = 0; //initial time
+int slid = 5; //initial slide
 
-void allocaBuffer(int ts){ //alloca il Buffer, che inizialmente sarà vuoto e dunque la testa sarà = NULL e il numero di elementi presenti sarà zero
+void allocateBuffer(int ts){ // it allocate the buffer, at the beginning it will be empty and the head will be NULL and the number of elements will be zero
     if (x==NULL) {
         x = malloc(sizeof *x);
-        x->head = malloc(MM*(sizeof (x->head)));
+        x->head = malloc(Table_Length*(sizeof (x->head)));
         x->N = 0;
         x->M=0;
         x->c = (int) ceil((double) abs(ts- t) / (double) slid)*slid;
@@ -16,27 +16,27 @@ void allocaBuffer(int ts){ //alloca il Buffer, che inizialmente sarà vuoto e du
     }
 }
 
-void addToBuffer(char e[Num_String][String_Lenght], int ts, int l){ //aggiunge al Buffer
-    if (x->M>x->N){ //nel momento in cui N riparte da 0
-        while (l<MM) {
+void addToBuffer(char e[Num_String][String_Length], int ts, int l){ //add the content to the Buffer
+    if (x->M>x->N){ //in case N starts again from 0
+        while (l<Table_Length) {
             if (ts != x->head[l]->w.c) {
                 int k = x->N;
                 x->head[l]->nc++;
                 if (x->head[l]->c ==
-                    NULL) { //se nella finestra non è già presente un conenuto allora vado ad allocare lo spazio da zero
+                    NULL) { //if the window is empty of content i will allocate space
                     x->head[l]->c = malloc(sizeof(*x->head[l]->c));
 
-                } else { //se è già presente del contenuto aumento lo spazio dell'allocazione dinamica
+                } else { //if in the window there is some content i will increase the allocation space
                     x->head[l]->c = realloc(x->head[l]->c, (x->head[l]->nc) * (sizeof(*x->head[l]->c)));
 
                 }
                 x->head[l]->c[x->head[l]->nc - 1].e = malloc(Num_String*sizeof(char*));
                 for (int i=0;i<Num_String;i++)
-                    x->head[l]->c[x->head[l]->nc - 1].e[i] = malloc(String_Lenght*sizeof(char));
+                    x->head[l]->c[x->head[l]->nc - 1].e[i] = malloc(String_Length*sizeof(char));
                 for (int p=0;p<Num_String;p++)
-                    for (int t=0;t<String_Lenght;t++)
-                        x->head[l]->c[x->head[l]->nc - 1].e[p][t] = e[p][t]; // indica l'ennesimo content di quella finestra
-                x->head[l]->c[x->head[l]->nc - 1].ts = ts;// indica l'ennesimo timestamp di quella finestra
+                    for (int t=0;t<String_Length;t++)
+                        x->head[l]->c[x->head[l]->nc - 1].e[p][t] = e[p][t]; //it indicates the umpteenth element of that window
+                x->head[l]->c[x->head[l]->nc - 1].ts = ts;//it indicates the umpteenth timestamp of that window
             }l++;
         }
         l = 0;
@@ -44,19 +44,19 @@ void addToBuffer(char e[Num_String][String_Lenght], int ts, int l){ //aggiunge a
             if (ts != x->head[l]->w.c) {
                 x->head[l]->nc++;
                 if (x->head[l]->c ==
-                    NULL) { //se nella finestra non è già presente un conenuto allora vado ad allocare lo spazio da zero
+                    NULL) { //if the window is empty of content i will allocate space
                     x->head[l]->c = malloc(sizeof(*x->head[l]->c));
-                } else { //se è già presente del contenuto aumento lo spazio dell'allocazione dinamica
+                } else { //if in the window there is some content i will increase the allocation space
                     x->head[l]->c = realloc(x->head[l]->c, (x->head[l]->nc) * (sizeof(*x->head[l]->c)));
 
                 }
                 x->head[l]->c[x->head[l]->nc - 1].e = malloc(Num_String*sizeof(char*));
                 for (int i=0;i<Num_String;i++)
-                    x->head[l]->c[x->head[l]->nc - 1].e[i] = malloc(String_Lenght*sizeof(char));
+                    x->head[l]->c[x->head[l]->nc - 1].e[i] = malloc(String_Length*sizeof(char));
                 for (int p=0;p<Num_String;p++)
-                    for (int t=0;t<String_Lenght;t++)
-                        x->head[l]->c[x->head[l]->nc - 1].e[p][t] = e[p][t]; // indica l'ennesimo content di quella finestra
-                x->head[l]->c[x->head[l]->nc - 1].ts = ts;// indica l'ennesimo timestamp di quella finestra
+                    for (int t=0;t<String_Length;t++)
+                        x->head[l]->c[x->head[l]->nc - 1].e[p][t] = e[p][t]; //it indicates the umpteenth element of that window
+                x->head[l]->c[x->head[l]->nc - 1].ts = ts;//it indicates the umpteenth timestamp of that window
             }l++;
 
         }
@@ -66,20 +66,21 @@ void addToBuffer(char e[Num_String][String_Lenght], int ts, int l){ //aggiunge a
             if (ts != x->head[l]->w.c) {
                 x->head[l]->nc++;
                 if (x->head[l]->c ==
-                    NULL) { //se nella finestra non è già presente un conenuto allora vado ad allocare lo spazio da zero
+                    NULL) { //if the window is empty of content i will allocate space
                     x->head[l]->c = malloc(sizeof(*x->head[l]->c));
-                } else { //se è già presente del contenuto aumento lo spazio dell'allocazione dinamica
+                } else { //if in the window there is some content i will increase the allocation space
                     x->head[l]->c = realloc(x->head[l]->c, (x->head[l]->nc) * (sizeof(*x->head[l]->c)));
                 }
                 x->head[l]->c[x->head[l]->nc - 1].e = malloc(Num_String*sizeof(char*));
                 for (int i=0;i<Num_String;i++)
-                    x->head[l]->c[x->head[l]->nc - 1].e[i] = malloc(String_Lenght*sizeof(char));
+                    x->head[l]->c[x->head[l]->nc - 1].e[i] = malloc(String_Length*sizeof(char));
                 for (int p=0;p<Num_String;p++)
-                    for (int t=0;t<String_Lenght;t++)
-                        x->head[l]->c[x->head[l]->nc - 1].e[p][t] ='s';// e[p][t]; // indica l'ennesimo content di quella finestra
-                x->head[l]->c[x->head[l]->nc - 1].ts = ts;// indica l'ennesimo timestamp di quella finestra
+                    for (int t=0;t<String_Length;t++)
+                        x->head[l]->c[x->head[l]->nc - 1].e[p][t] ='s';// e[p][t]; //it indicates the umpteenth element of that window
+                x->head[l]->c[x->head[l]->nc - 1].ts = ts;//it indicates the umpteenth timestamp of that window
             }l++;
         }
     }
 }
+
 
